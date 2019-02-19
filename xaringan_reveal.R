f7e94200acecbd35d4ce9937d9709017f5b1397e
EvaMaeRey <evangeline.mae@gmail.com>
2019-02-19 13:35:44 -0700

    First commit.

---
+++ xaringan_reveal.R
@@ -0,0 +1,29 @@
+# reveal lines up to `upto` and highlight lines `highlight`
+reveal <- function(name, upto, highlight = upto) {
+  content <- knitr:::knit_code$get(name)
+  content[upto] <- gsub("%>%", "", content[upto], fixed = T)
+  content[upto] <- gsub("->", "", content[upto], fixed = T)
+  content[upto] <- gsub("+", "", content[upto], fixed = T)
+  content[highlight] <- paste(content[highlight], "#<<")
+  content[1:upto]
+}
+
+partial_knit_chunks <- function(chunk_name) {
+  # Create slide for lines 1:N for each line N in the given chunk
+  idx_lines <- seq_along(knitr:::knit_code$get(chunk_name))
+  partial_knit_steps <- glue::glue(
+    "class: split-40",
+    "count: false",
+    "",
+    ".column[.content[",
+    "```{r plot_{{chunk_name}}_{{idx_lines}}, eval=FALSE, code=reveal('{{chunk_name}}', {{idx_lines}})}",
+    "```",
+    "]]",
+    ".column[.content.center[",
+    "```{r output_{{chunk_name}}_{{idx_lines}}, echo=FALSE, code=reveal('{{chunk_name}}', {{idx_lines}})}",
+    "```",
+    "]]",
+    .open = "{{", .close = "}}", .sep = "\n"
+  )
+  glue::glue_collapse(partial_knit_steps, "\n---\n")
+}
